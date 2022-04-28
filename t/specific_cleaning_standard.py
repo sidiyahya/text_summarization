434e778862fa085a9ff8ed4f6cccd6b446fa6916
Ali <mohamedali.elhadj@gmail.com>
2022-04-28 21:22:21 +0200

    last modifs

--- t/specific_cleaning_standard.py
+++ t/specific_cleaning_standard.py
@@ -1,26 +1,30 @@
 #pattern [from, [index, length, regex]*n]
-def clean_standard_articles(text_per_page):
-    per_page_standard = [1, [0], ["last", 20]]
-    return clean_specific_text_at_position(text_per_page, per_page_standard)
+
+#['form', 'bold', regex]
+#['startwith, 'text', list]
+import copy
 
 
-def clean_specific_text_at_position(text_per_page, per_page):
-    per_page_ = per_page[:1] + find_last(text_per_page, per_page[1:])
-    cleaned_text = [text_per_page[0]] if(per_page_[0]==1) else []
-    for i in range(per_page_[0], len(text_per_page) - 1):
-        text = text_per_page[i]
-        for ignored_text in per_page_[1:]:
-            index_to_remove = ignored_text[0]
-            if(len(ignored_text)==1):
-                del text[index_to_remove]
-            elif(len(ignored_text)==2):
-                len_index_to_remove = ignored_text[1]
-                if(len(text[index_to_remove])<len_index_to_remove):
-                    del text[index_to_remove]
+def clean_standard_articles(text_per_page, config_per_page):
+    return clean_specific_text_at_position(text_per_page, config_per_page)
 
-        cleaned_text.append(text)
 
-    return cleaned_text
+def clean_specific_text_at_position(text_per_page, config_per_page):
+    config_per_page = find_last(text_per_page, config_per_page)
+    indexes_to_remove = []
+    for ignored_text in config_per_page:
+        index_to_remove = ignored_text[0]
+        if(len(ignored_text)==1):
+            indexes_to_remove.append(index_to_remove)
+            #del text_per_page[index_to_remove]
+        elif(len(ignored_text)==2):
+            len_index_to_remove = ignored_text[1]
+            if(len(text_per_page[index_to_remove])<len_index_to_remove):
+                indexes_to_remove.append(index_to_remove)
+                #del text_per_page_cleaned[index_to_remove]
+
+    text_per_page_cleaned = [text_per_page[i] for i in range(len(text_per_page)) if(i not in indexes_to_remove)]
+    return text_per_page_cleaned
 
 def find_last(text, per_page):
     last = len(text)-1
