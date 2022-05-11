8ce29030a3a44a46293b972e53684a998c1db3a4
sidiyahya <mohamedali.elhadj@gmail.com>
2022-05-11 21:15:38 +0200

    cleaning code

--- output_methods/sortie_wordcloud.py
+++ output_methods/sortie_wordcloud.py
@@ -13,6 +13,30 @@ def run_wordcloud(text):
     plt.show()
 
 
+### Création du nuage de mot
+def nuage():
+    words = nltk.word_tokenize(tf)  # On tokenise tous les mots
+    stops = set(stopwords.words('english'))  # On enlève les stopwords
+    wordsFiltered = []
+
+    ## Boucle qui sert à récuperer que les mots qui contiennent plus de 2 caractères
+    for w in words:
+        if w not in stops:
+            if len(w) > 2:
+                if w.isalpha() == True:
+                    wordsFiltered.append(w)
+
+    lemmatizer = WordNetLemmatizer()  # On lemmatise les mots
+
+    for w in range(0, len(wordsFiltered)):
+        wordsFiltered[w] = lemmatizer.lemmatize(wordsFiltered[w])
+
+    text = " ".join(wordsFiltered)
+    wordcloud = WordCloud(background_color='white', stopwords=stops, max_words=50, width=1600, height=800).generate(
+        text)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.savefig(str(lien + 'nuagearticles.png'), dpi=1500)  # Extraction du nuage de mot
 
 if __name__ == '__main__':
     file = input("Entrez le chemin de votre fichier: ")
--- output_methods/sortis_orchestre.py
+++ output_methods/sortis_orchestre.py
@@ -13,3 +13,16 @@ if __name__ == '__main__':
     text = read_text_file(file)
     summary_nltk, summary_spacy = run_algorithms(text)
     sortis_orchestre(summary_nltk)
+if __name__ == '__main__':
+    repertoire = input("Entrez le repertoire des fichiers: ")
+    dossier_path = os.path.join(os.path.dirname(".."), repertoire)
+    for file_path in os.listdir(dossier_path):
+        with open(os.path.join(dossier_path, file_path), 'r') as f:
+            text = f.read()
+        ### Création du nuage de mot
+        create_graph(text.split("\n"))
+        nuage(text)
+        ####################
+        # Création du graph neural
+        ####################
+        create_graph(text.split("\n"))
