d6832e7d76c82ccd142b407f69ddfc0862b209a9
sidiyahya <mohamedali.elhadj@gmail.com>
2022-05-29 02:51:54 +0200

    data reconstruction for dash cytoscape

--- ail/reconstruct_data.py
+++ ail/reconstruct_data.py
@@ -28,6 +28,22 @@ def reconstruct_data(df):
 
     data_reconstructed_list = [[i] + list(j) for i, j in zip(tuple(data_reconstructed.keys()), tuple(data_reconstructed.values()))]
     df_reconstructed = pd.DataFrame(data=data_reconstructed_list, columns=['source', 'targets', 'weights'])
+    df_reconstructed["n_cites"] = pd.Series(dtype='int')
+    df_reconstructed["cited_by"] = ""
+
+    for index, row in df_reconstructed.iterrows():
+        targets = row['targets']
+        targets_id = ""
+        n_cites = 0
+        for target in targets:
+            if(df_reconstructed.index[df_reconstructed['source'] == target].tolist()):
+                id = df_reconstructed.index[df_reconstructed['source'] == target].tolist()[0]
+                targets_id += str(id) if(len(targets_id)==0) else ","+str(id)
+                n_cites+=1
+
+        df_reconstructed.at[index, "cited_by"] = targets_id
+        df_reconstructed.at[index, "n_cites"] = int(n_cites)
+
     return df_reconstructed
 
 
@@ -37,5 +53,7 @@ if __name__ == '__main__':
     f = open('positions.json')
     positions = json.load(f)
     df = reconstruct_data(links_filtered)
+    df["n_cites"] = df["n_cites"].astype(int)
+
     df_final = add_x_y(df, positions)
     df.to_csv('df_final.csv', encoding='utf-8', index=False)
